7e8564fdf1f98a58f6b09e58f2a8204312c23ad5
ReganAtAerion <regan@aerion.com.au>
2024-09-22 15:04:11 +0545

    changes made

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 border-4 border-blue-700 justify-center items-center">
-      <Text className="text-orange-400 text-4xl">Edit app/index.tsx to edit this screens.</Text>
+      <Text className="text-orange-400 text-4xl">Edit *app/index.tsx* to edit this screens.</Text>
     </View>
   );
 }
