6e0307092efa3e98da9b2e3eb41495467a9cc959
ReganAtAerion <regan@aerion.com.au>
2024-09-22 14:56:03 +0545

    feat: commitlint with husky and few more changes

---
+++ commitlint.config.ts
@@ -0,0 +1,27 @@
+// export default { extends: ["@commitlint/config-conventional"] };
+
+import type { UserConfig } from "@commitlint/types";
+
+const Configuration: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // New features
+        "fix", // Bug fixes
+        "chore", // Maintenance tasks
+        "ci", // CI configuration changes
+        "docs", // Documentation updates
+        "perf", // Performance improvements
+        "refactor", // Code refactoring
+        "revert", // Revert to previous commit
+        "style", // Code style changes
+        "test", // Adding or updating tests
+      ],
+    ],
+  },
+};
+
+module.exports = Configuration;
