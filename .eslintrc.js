ce635edba9e1a7e9c535a38fc4fd2423ade9dc23
ReganAtAerion <regan@aerion.com.au>
2024-10-27 15:11:36 +0545

    add: packg and config added

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,6 @@ module.exports = {
     "prettier/prettier": "off",
     // "@typescript-eslint/explicit-module-boundary-types": "off",
     // "import/no-useless-path-segments": "off",
-    // "no-unused-vars": "off",
+    "no-unused-vars": "warn",
   },
 };
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,31 @@
-import { Stack } from "expo-router";
 import { Provider } from "react-redux";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Slot, SplashScreen, Stack } from "expo-router";
 
 import "../global.css";
 import { store } from "@/redux/store";
+// import mockApi from "@/services/api/mock";
+import { AppConfiguration } from "@/components";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+SplashScreen.preventAutoHideAsync();
+
+// mockApi({ delayResponse: 2000, onNoMatch: "passthrough" });
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
       <Provider store={store}>
-        <Stack
+        <AppConfiguration>
+          <Slot />
+
+          {/* <Stack
           screenOptions={{
             headerShown: false,
           }}>
           <Stack.Screen name="(auth)" />
           <Stack.Screen name="(protected)" />
-        </Stack>
+        </Stack> */}
+        </AppConfiguration>
       </Provider>
     </SafeAreaProvider>
   );
